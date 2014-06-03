347861ce4a5d33dd41a28c0f5eb642afa4f9ef5c
Jeff Magnusson <jmagnusson@netflix.com>
2014-06-02 17:13:56 -0700

    initial commit

---
+++ _vim/.vim
@@ -0,0 +1,1 @@
+/Users/jmagnusson/code/dotfiles/_vim
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+function clean_link_file {
+    source="${PWD}/$1"
+    target="${2}/${1/_/.}"
+	link_file $source $target
+}
+
+function link_file {
+	source=$1
+	target=$2
+
+    if [ -e "${target}" ]; then
+        mv $target $target.bak
+    fi
+
+    ln -sf ${source} ${target}
+}
+
+for i in _*
+do
+	clean_link_file $i $1
+done
+
+source="${PWD}/_vim/_vimrc"
+target="${1}/.vimrc"
+link_file $source $target 
+
+cat submodules | perl -e 'while(<>){($a,$b) = split(/\s+/, $_); print `git submodule add $a $b`;}'
+
+git reset
