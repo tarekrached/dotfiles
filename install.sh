20e37c2c96304594f66cfec2e14d3c0c95658630
Tarek Rached <trached@stitchfix.com>
2015-10-29 12:01:27 -0700

    mine all mine

--- install.sh
+++ install.sh
@@ -23,8 +23,23 @@ done
 
 source="${PWD}/_vim/_vimrc"
 target="${1}/.vimrc"
-link_file $source $target 
+link_file $source $target
 
-cat submodules | perl -e 'while(<>){($a,$b) = split(/\s+/, $_); print `git submodule add $a $b`;}'
+# cat submodules | perl -e 'while(<>){($a,$b) = split(/\s+/, $_); print `git submodule add $a $b`;}'
 
-git reset
+# git reset
+
+
+# do vundle install for vim plugins
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+:BundleInstall
+
+# do install prezto https://github.com/sorin-ionescu/prezto
+# sudo apt-get install zsh
+# zsh
+git clone --recursive https://github.com/tarekrached/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+# sudo chsh ubuntu -s /bin/zsh
